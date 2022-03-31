3cbda83345c9b1f54219c970ee79d6ae13f04fb8
Almehdi KRISNI <almehdi.krisni@gmail.com>
2022-03-31 17:46:34 +0200

    First scraping OK - To complete on website (100 values to start)

--- scrap.py
+++ scrap.py
@@ -4,22 +4,50 @@
 ###################################################################### IMPORTS ################################################################################
 
 # Necessary imports
+from pyrsistent import v
 import requests as rq
 import lxml
 from bs4 import BeautifulSoup as bs
+import string
 
-############################################################## CREATION, ACCESS AND PARSING ####################################################################
+from torch import nuclear_norm
+
+######################################################### CREATION, ACCESS, PARSING, EXTRACTION ###############################################################
 
 # URL creation and access
-url = "https://www.allrecipes.com"
-f = rq.get(url)
+url = "https://www.allrecipes.com/recipe/109291/"
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
+}
+f = rq.get(url, headers=headers)
 
 # Parse webpage
-dt = bs(f.content, 'lxml')
+recipe = bs(f.content, 'lxml')
 
-###################################################################### TEST PART ################################################################################
-# Information extraction
-test = dt.find('table')
+# Researched values
+research = ["fat", "saturated fat", "sodium", "sugars"]
 
-# Print test
-dt[0]
+# Data extraction
+# Dict creation
+nutrVal = dict()
+
+# Recipe name
+recipe_name = recipe.find('h1', {"class" : "headline heading-content elementFont__display"}).text
+nutrVal["recipe name"] = recipe_name
+
+# Nutritional info
+recipe_nutr_info = recipe.find_all('div', {"class" : "nutrition-row"})
+
+
+for i in recipe_nutr_info :
+    res = i.find_all('span', {"class" : "nutrient-name"})
+
+    for j in res :
+        d = j.find('span', {"class" : "elementFont__details--bold elementFont__transformCapitalize"}).text.strip()
+        v = j.find('span', {"class" : "nutrient-value"}).text.strip()
+
+        if d in research :
+            nutrVal[d] = v
+
+
+print(nutrVal)

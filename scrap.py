3dff6d9b0d71f19b9d9a9438e30d3c5acaa9fbf8
Almehdi KRISNI <almehdi.krisni@gmail.com>
2022-03-31 15:34:39 +0200

    Scraper started

---
+++ scrap.py
@@ -0,0 +1,24 @@
+# Web scraper in Python
+# Tutorial from - https://www.topcoder.com/thrive/articles/web-crawler-in-python
+
+###################################################################### IMPORTS ################################################################################
+
+# Necessary imports
+import requests as rq
+import lxml
+from bs4 import BeautifulSoup as bs
+
+############################################################## CREATION, ACCESS AND PARSING ####################################################################
+
+# URL creation and access
+url = "https://www.allrecipes.com"
+f = rq.get(url)
+
+# Parse webpage
+dt = bs(f.content, 'lxml')
+
+###################################################################### TEST PART ################################################################################
+# Information extraction
+test = dt.find('table')
+
+# Print test
